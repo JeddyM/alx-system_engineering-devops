cad666f764df22574f8f2cd482a6723f2f95130a
JeddyM <jeddymweru@gmail.com>
2023-08-17 20:34:29 +0000

    Fixing puppet-lint errors

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Increasing the number of requests nginx webserver can handle
- exec { 'increase_nginx_connections':
-	command     => 'sed -i "s/worker_connections [0-9]\+/worker_connections 2048/" /etc/nginx/nginx.conf',
-	path        => '/bin:/usr/bin',
-	refreshonly => true,
-	notify      => Service['nginx'],
+exec { 'increase_nginx_connections':
+  command     => 'sed -i "s/worker_connections [0-9]\+/worker_connections 2048/" /etc/nginx/nginx.conf',
+  path        => '/bin:/usr/bin',
+  refreshonly => true,
+  notify      => Service['nginx'],
 }
